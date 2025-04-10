196003bc5ef908cb18021620344466e4bb615a86
fatihGunal <fatihgunal2018@gmail.com>
2025-04-10 02:29:39 +0200

    Finishing lexer tokenizing code and create repl

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/fatihGunal/monkey-compiler/internal/repl"
 )
 
 func main() {
-	fmt.Println("Hello World make file!")
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+	fmt.Printf("Feel free to type in command \n")
+	repl.Start(os.Stdin, os.Stdout)
 }
---
+++ internal/repl/repl.go
@@ -0,0 +1,32 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+
+	"github.com/fatihGunal/monkey-compiler/internal/lexer"
+	"github.com/fatihGunal/monkey-compiler/internal/token"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+
+		l := lexer.New(line)
+
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+}
